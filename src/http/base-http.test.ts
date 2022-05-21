2ce6898ee87878dae4b010e63201473721cd547b
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-05-21 23:26:11 +0200

    Migrate base-http.spec.ts to std/testing/bdd

--- src/http/base-http.test.ts
+++ src/http/base-http.test.ts
@@ -1,19 +1,21 @@
-import { SerializerNodejsImpl } from '../serializers/index.ts';
+import { SerializerNativeImpl } from '../serializers/index.ts';
 import { BaseHttp } from './base-http.ts';
+import { assertEquals } from 'https://deno.land/std@0.140.0/testing/asserts.ts';
+import { describe, it } from 'https://deno.land/std@0.140.0/testing/bdd.ts';
 
 class Test extends BaseHttp {
   config = {
     url: 'https://mastodon.social',
     accessToken: 'token',
   };
-  request = jest.fn();
-  serializer = new SerializerNodejsImpl();
+  request = (() => {}) as any;
+  serializer = new SerializerNativeImpl();
 }
 
 describe('BaseHttp', () => {
   it('creates header', () => {
     const test = new Test();
-    expect(test.createHeader({ extra: 'header' })).toEqual({
+    assertEquals(test.createHeader({ extra: 'header' }), {
       Authorization: 'Bearer token',
       'Content-Type': 'application/json',
       extra: 'header',
@@ -22,9 +24,7 @@ describe('BaseHttp', () => {
 
   it('override content-type header', () => {
     const test = new Test();
-    expect(
-      test.createHeader({ 'Content-Type': 'multipart/form-data' }),
-    ).toEqual({
+    assertEquals(test.createHeader({ 'Content-Type': 'multipart/form-data' }), {
       Authorization: 'Bearer token',
       'Content-Type': 'multipart/form-data',
     });
@@ -32,14 +32,16 @@ describe('BaseHttp', () => {
 
   it('resolves url', () => {
     const test = new Test();
-    expect(test.resolveUrl('/api/v1/yay')).toEqual(
+    assertEquals(
+      test.resolveUrl('/api/v1/yay'),
       'https://mastodon.social/api/v1/yay',
     );
   });
 
   it('resolves url with params', () => {
     const test = new Test();
-    expect(test.resolveUrl('/api/v1/yay', { query: true })).toEqual(
+    assertEquals(
+      test.resolveUrl('/api/v1/yay', { query: true }),
       'https://mastodon.social/api/v1/yay?query=true',
     );
   });
